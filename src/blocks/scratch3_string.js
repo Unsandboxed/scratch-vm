5aff15bc43c85dcb57b24169e81bca4cd123de10
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2024-03-04 03:36:02 +0000

    Update scratch3_string.js

--- src/blocks/scratch3_string.js
+++ src/blocks/scratch3_string.js
@@ -101,9 +101,9 @@ class Scratch3StringBlocks {
     }
 
     _getIndexFromSplit (string, split, index) { // used by compiler
-        if (index.toLowerCase() === "last") {
+        if (index === "last") {
             index = string.length - 1;
-        } else if (index.toLowerCase() === "random") {
+        } else if (index === "random") {
             index = Math.floor(Math.random()*string.length);
         } else {
             index = Cast.toNumber(index) - 1;
