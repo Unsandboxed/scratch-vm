2468946787bf20bb8468c774bb1e3e78cf35f4bc
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-10-06 22:12:17 -0400

    Please work

--- src/blocks/scratch3_control.js
+++ src/blocks/scratch3_control.js
@@ -151,11 +151,44 @@ class Scratch3ControlBlocks {
         }
     }
 
-    break (args, util) {
-        util.thread.breakThisLoop();
+    // break and continue code borrowed from
+    // https://github.com/surv-is-a-dev/gallery/blob/main/site-files/extensions/0znzw/tests/breakContinue.js
+    _getLoopFrame(util) {
+        const thread = util.thread, stackFrames = thread.stackFrames, frameCount = stackFrames.length;
+        let loopFrameBlock = null, loopFrameIndex = -1;
+        for (let i = frameCount - 1; i >= 0; i--) {
+            if (!stackFrames[i].isLoop) continue;
+            loopFrameBlock = stackFrames[i].op.id;
+            loopFrameIndex = i;
+            break;
+        }
+        if (!loopFrameBlock) return false;
+        return [loopFrameBlock, loopFrameIndex];
     }
-
-    continue (args, util) {
+    break (_, util) {
+        const thread = util.thread, stackFrame = thread.peekStackFrame();
+        if (!stackFrame._breakData) {
+            let frameData = false;
+            if (!(frameData = this._getLoopFrame(util))) return console.warn('Not in a loop!');
+            const loopFrameBlock = frameData[0];
+            const afterLoop = thread.blockContainer.getBlock(loopFrameBlock).next;
+            if (!afterLoop) return;
+            stackFrame._breakData = { loopFrameBlock, afterLoop };
+        }
+        const { loopFrameBlock, afterLoop } = stackFrame._breakData;
+        while(thread.stack.at(-1) !== loopFrameBlock) thread.popStack();
+        thread.popStack();
+        thread.pushStack(afterLoop);
+    }
+    continue (_, util) {
+        const thread = util.thread, blocks = thread.blockContainer, stackFrame = thread.peekStackFrame();
+        if (!stackFrame._continueData) {
+        let frameData = false;
+        if (!(frameData = this._getLoopFrame(util))) return console.warn('Not in a loop!');
+            stackFrame._continueData = frameData[0];
+        }
+        while(thread.stack.at(-1) !== stackFrame._continueData) thread.popStack();
+        thread.status = thread.constructor.STATUS_YIELD;
     }
 
     createClone (args, util) {
