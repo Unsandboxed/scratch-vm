79c53b897cc3fd861951053629b877864daca28b
yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>
2025-03-04 07:21:34 -0500

    implement camera

--- src/compiler/setup-compiler/camera.js
+++ src/compiler/setup-compiler/camera.js
@@ -6,48 +6,50 @@ module.exports = function (compilerData, {
 }) {
     /* eslint-disable no-invalid-this,prefer-arrow-callback */
     // Stack
-    compilerData.registerBlock('camera_changex', function (stg, block) {
-        return new IntermediateStackBlock(this.ir_opcode, {
-            dx: stg.descendInputOfBlock(block, 'DX').toType(InputType.NUMBER)
-        });
-    }, null, {
-        input: false
-    });
-    compilerData.registerBlock('camera_changey', function (stg, block) {
-        return new IntermediateStackBlock(this.ir_opcode, {
-            dy: stg.descendInputOfBlock(block, 'DY').toType(InputType.NUMBER)
-        });
-    }, null, {
-        input: false
-    });
-    compilerData.registerBlock('camera_changebyxy', function (stg, block) {
-        return new IntermediateStackBlock(this.ir_opcode, {
-            dx: stg.descendInputOfBlock(block, 'DX').toType(InputType.NUMBER),
-            dy: stg.descendInputOfBlock(block, 'DY').toType(InputType.NUMBER)
-        });
-    }, null, {
-        input: false
-    });
-    compilerData.registerBlock('camera_movetoxy', function (stg, block) {
-        return new IntermediateStackBlock(this.ir_opcode, {
-            x: stg.descendInputOfBlock(block, 'X').toType(InputType.NUMBER),
-            y: stg.descendInputOfBlock(block, 'Y').toType(InputType.NUMBER)
-        });
-    }, null, {
-        input: false
-    });
-    compilerData.registerBlock('camera_setx', function (stg, block) {
-        return new IntermediateStackBlock(this.ir_opcode, {
-            x: stg.descendInputOfBlock(block, 'X').toType(InputType.NUMBER)
-        });
-    }, null, {
-        input: false
-    });
-    compilerData.registerBlock('camera_sety', function (stg, block) {
-        return new IntermediateStackBlock(this.ir_opcode, {
-            y: stg.descendInputOfBlock(block, 'Y').toType(InputType.NUMBER)
-        });
-    }, null, {
+    compilerData.registerBlock([
+        'camera_setx',
+        'camera_sety',
+        'camera_movetoxy',
+        'camera_changex',
+        'camera_changey',
+        'camera_changebyxy'
+    ], function (stg, block) {
+        const args = {};
+        switch (this.ir_opcode) {
+        case 'camera.setx':
+        case 'camera.sety':
+        case 'camera.movetoxy':
+            if ('X' in block.inputs) args.x = stg.descendInputOfBlock(block, 'X').toType(InputType.NUMBER);
+            if ('Y' in block.inputs) args.y = stg.descendInputOfBlock(block, 'Y').toType(InputType.NUMBER);
+            break;
+        case 'camera.changex':
+        case 'camera.changey':
+        case 'camera.changebyxy': {
+            args.disableModulo = true;
+            const DX = `X`;
+            const DY = `Y`;
+            if (DX in block.inputs) {
+                args.x = new IntermediateInput(`operator.add`, InputType.NUMBER, {
+                    left: new IntermediateInput('camera.xposition', InputType.NUMBER),
+                    right: stg.descendInputOfBlock(block, DX).toType(InputType.NUMBER)
+                });
+            } else args.x = new IntermediateInput('camera.xposition', InputType.NUMBER);
+            if (DY in block.inputs) {
+                args.y = new IntermediateInput(`operator.add`, InputType.NUMBER, {
+                    left: new IntermediateInput('camera.yposition', InputType.NUMBER),
+                    right: stg.descendInputOfBlock(block, DY).toType(InputType.NUMBER)
+                });
+            } else args.y = new IntermediateInput('camera.yposition', InputType.NUMBER);
+            break;
+        }
+        }
+        return new IntermediateStackBlock('camera.movetoxy', args);
+    }, function (jsg, block) {
+        jsg.descendedIntoModulo = false;
+        const x = 'x' in block.inputs ? jsg.descendInput(block.inputs.x) : 'runtime.camera.x';
+        const y = 'y' in block.inputs ? jsg.descendInput(block.inputs.y) : 'runtime.camera.y';
+        jsg.source += `runtime.camera.setXY(${x}, ${y});\n`;
+    }, {
         input: false
     });
     // Inputs
