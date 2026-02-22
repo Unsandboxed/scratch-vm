6103ad9de8f1f202e2f9d9f00aba9275d537a183
Miyo Sho <135030944+yuri-kiss@users.noreply.github.com>
2026-02-21 19:13:34 -0500

    no more "runtime.constructor"

--- src/io/ble.js
+++ src/io/ble.js
@@ -1,3 +1,4 @@
+const Runtime = require('../engine/runtime');
 const JSONRPC = require('../util/jsonrpc');
 
 class BLE extends JSONRPC {
@@ -60,7 +61,7 @@ class BLE extends JSONRPC {
         this.sendRemoteRequest('connect', {peripheralId: id})
             .then(() => {
                 this._connected = true;
-                this._runtime.emit(this._runtime.constructor.PERIPHERAL_CONNECTED);
+                this._runtime.emit(Runtime.PERIPHERAL_CONNECTED);
                 this._connectCallback();
             })
             .catch(e => {
@@ -85,7 +86,7 @@ class BLE extends JSONRPC {
         }
 
         // Sets connection status icon to orange
-        this._runtime.emit(this._runtime.constructor.PERIPHERAL_DISCONNECTED);
+        this._runtime.emit(Runtime.PERIPHERAL_DISCONNECTED);
     }
 
     /**
@@ -173,7 +174,7 @@ class BLE extends JSONRPC {
         case 'didDiscoverPeripheral':
             this._availablePeripherals[params.peripheralId] = params;
             this._runtime.emit(
-                this._runtime.constructor.PERIPHERAL_LIST_UPDATE,
+                Runtime.PERIPHERAL_LIST_UPDATE,
                 this._availablePeripherals
             );
             if (this._discoverTimeoutID) {
@@ -183,7 +184,7 @@ class BLE extends JSONRPC {
         case 'userDidPickPeripheral':
             this._availablePeripherals[params.peripheralId] = params;
             this._runtime.emit(
-                this._runtime.constructor.USER_PICKED_PERIPHERAL,
+                Runtime.USER_PICKED_PERIPHERAL,
                 this._availablePeripherals
             );
             if (this._discoverTimeoutID) {
@@ -192,7 +193,7 @@ class BLE extends JSONRPC {
             break;
         case 'userDidNotPickPeripheral':
             this._runtime.emit(
-                this._runtime.constructor.PERIPHERAL_SCAN_TIMEOUT
+                Runtime.PERIPHERAL_SCAN_TIMEOUT
             );
             if (this._discoverTimeoutID) {
                 window.clearTimeout(this._discoverTimeoutID);
@@ -230,7 +231,7 @@ class BLE extends JSONRPC {
             this._resetCallback();
         }
 
-        this._runtime.emit(this._runtime.constructor.PERIPHERAL_CONNECTION_LOST_ERROR, {
+        this._runtime.emit(Runtime.PERIPHERAL_CONNECTION_LOST_ERROR, {
             message: `Scratch lost connection to`,
             extensionId: this._extensionId
         });
@@ -239,7 +240,7 @@ class BLE extends JSONRPC {
     _handleRequestError (/* e */) {
         // log.error(`BLE error: ${JSON.stringify(e)}`);
 
-        this._runtime.emit(this._runtime.constructor.PERIPHERAL_REQUEST_ERROR, {
+        this._runtime.emit(Runtime.PERIPHERAL_REQUEST_ERROR, {
             message: `Scratch lost connection to`,
             extensionId: this._extensionId
         });
@@ -249,7 +250,7 @@ class BLE extends JSONRPC {
         if (this._discoverTimeoutID) {
             window.clearTimeout(this._discoverTimeoutID);
         }
-        this._runtime.emit(this._runtime.constructor.PERIPHERAL_SCAN_TIMEOUT);
+        this._runtime.emit(Runtime.PERIPHERAL_SCAN_TIMEOUT);
     }
 }
 
